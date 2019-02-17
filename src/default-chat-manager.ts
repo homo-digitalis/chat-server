de03ec2508c7299a1eafa6109aaeb4f0eeabf3e8
homo-digitalis <homo-digitalis@gmx.de>
2019-02-17 10:17:19 +0100

    consistency

--- src/default-chat-manager.ts
+++ src/default-chat-manager.ts
@@ -1,7 +1,7 @@
+import { CurriculaService } from "@homo-digitalis/curricula"
 import { HomoDigitalis } from "homo-digitalis"
-import { CurriculaService } from "homo-digitalis-curricula-service"
 import { IIntent } from "nlp-trainer"
-import { IAnswer, IAnswerExtended } from "nlp-with-actions"
+import { IAnswer } from "nlp-with-actions"
 import { IChatAdministrator } from "./chat-server"
 
 export class DefaultChatManager implements IChatAdministrator {
@@ -15,12 +15,14 @@ export class DefaultChatManager implements IChatAdministrator {
         console.log(`user connected ${socket.id}`)
         this.authenticatedSocketIDs.push(socket.id)
 
-        const curriculumContent: IIntent[] = await this.curriculaService.provideCurriculumByID("exampleMap")
+        const curriculumContent: IIntent[] =
+            await this.curriculaService.provideCurriculumByID("exampleMap")
+
         await this.homoDigitalis.learn(curriculumContent)
     }
 
     public handleDisConnect(socket: any): void {
-        console.log("user disconnected")
+        console.log(`user disconnected: ${socket.id}`)
     }
 
     public async handleMessage(socketID: string, io: any, message: any): Promise<void> {
@@ -28,7 +30,6 @@ export class DefaultChatManager implements IChatAdministrator {
         if (this.authenticatedSocketIDs.some((authenticatedSocketID: string) => authenticatedSocketID === socketID)) {
             io.emit("message", { type: "message", text: message })
             if (!this.preparedSocketIDs.some((preparedSocketID: string) => preparedSocketID === socketID)) {
-                io.emit("message", { type: "message", text: "Yay. Das ist richtig. Ich wünsche Dir viel Spaß." })
                 this.preparedSocketIDs.push(socketID)
             }
             const answer: IAnswer = await this.homoDigitalis.answer(message)
@@ -38,8 +39,8 @@ export class DefaultChatManager implements IChatAdministrator {
 
     public async handleTrainingData(chatBotName: string, trainingData: any): Promise<void> {
         console.log(`TrainingData received: ${JSON.stringify(trainingData)}`)
-        await this.curriculaService.saveCurriculumByID("new2", trainingData)
-        const curriculumContent: IIntent[] = await this.curriculaService.provideCurriculumByID("new2")
+        await this.curriculaService.saveCurriculumByID(chatBotName, trainingData)
+        const curriculumContent: IIntent[] = await this.curriculaService.provideCurriculumByID(chatBotName)
         await this.homoDigitalis.learn(curriculumContent)
     }
 
